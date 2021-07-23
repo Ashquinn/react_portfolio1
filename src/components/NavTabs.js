ee9864038216cbf354aec623adb6ae44bde9aba6
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-23 17:09:51 -0400

    insertd an href for #project

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -18,7 +18,7 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#about"
+          href="#project"
           onClick={() => handlePageChange('Project')}
           // Check to see if the currentPage is `Project`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Project' ? 'nav-link active' : 'nav-link'}
