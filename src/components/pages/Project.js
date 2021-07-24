1e28453393e684f7b632fc42c92516616283dcde
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-23 20:41:03 -0400

    all project images, github links, and deployed links added to Project.js file

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,17 +1,122 @@
 import React from 'react';
+// import '../../styles/Projects.css';
 
-export default function Project() {
+// images and gifs imports
+
+import movieNight from '../../assets/images/movie_night_img.png';
+import teamManage from '../../assets/images/team_manage_sys.png';
+import readMe from '../../assets/images/README_Generator1.png';
+import weatherApp from '../../assets/images/weather_app3.png';
+import passGen from '../../assets/images/password2.png';
+import dayPlanner from '../../assets/images/scheduler2.png';
+
+export default function Projects() {
   return (
-    <div>
-      <h1>Projects Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div class="container work-body" id="work">
+    
+    <h1 class="assignmentsTitle" id="assignments">Projects <i class="fas fa-angle-double-left"></i></h1>
+    <hr class="hrAssignments"></hr>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+
+    <div class="col paddingTop">
+        <div class="card h-100 customCard">
+          <img src={movieNight} class="card-img-top group-img-height" alt="movie_night"/>
+          <div class="card-body group-card-body d-flex flex-column">
+            <h3 class="card-title"><ins>Movie Night Done Right</ins></h3>
+            <p class="card-text">This is a movie searching application that allows you to view movie details and find restuarants within your zipcode</p>
+          </div>
+          <div class="card-footer">
+            <div class="text-center mt-auto">
+              <a target="_blank" rel="noreferrer" href="https://github.com/AustinJoo97/MovieNightDoneRight" class="btn p-btn-color btn-lg custom-length-ps" role="button" aria-pressed="true"><b>GitHub </b><i class="fab fa-github-square"></i></a>
+              <a target="_blank" rel="noreferrer" href="https://austinjoo97.github.io/MovieNightDoneRight/" class="btn btn-secondary btn-lg custom-length-ss" role="button" aria-pressed="true"><i class="fas fa-window-maximize"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col paddingTop">
+        <div class="card h-100 customCard">
+          <img src={readMe} class="card-img-top group-img-height" alt="password_generator"/>
+          <div class="card-body group-card-body d-flex flex-column">
+            <h3 class="card-title"><ins>README Generator</ins></h3>
+            <p class="card-text h">This is a README generator that will produce a high quality readme.md file. Below are the links for the GitHub repository and webpage</p>
+          </div>
+          <div class="card-footer">
+            <div class="text-center mt-auto">
+              <a target="_blank" rel="noreferrer" href="https://github.com/Ashquinn/README_Generator" class="btn p-btn-color btn-lg custom-length-ps" role="button" aria-pressed="true"><b>GitHub </b><i class="fab fa-github-square"></i></a>
+              <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=mPq9EpREpBE" class="btn btn-secondary btn-lg  custom-length-ss" role="button" aria-pressed="true"><i class="fas fa-window-maximize"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col paddingTop">
+        <div class="card h-100 customCard">
+          <img src={teamManage} class="card-img-top group-img-height" alt="team_manage"/>
+          <div class="card-body group-card-body d-flex flex-column">
+            <h3 class="card-title"><ins>Team Management System</ins></h3>
+            <p class="card-text">This is a Team Management System that runs on Node.js. Below are the links for the GitHub repository and webpage</p>
+          </div>
+          <div class="card-footer">
+            <div class="text-center mt-auto">
+              <a target="_blank" rel="noreferrer" href="https://github.com/Ashquinn/team_management_system" class="btn p-btn-color btn-lg custom-length-ps" role="button" aria-pressed="true"><b>GitHub </b><i class="fab fa-github-square"></i></a>
+              <a target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=ljr-IS-p2EI" class="btn btn-secondary btn-lg custom-length-ss" role="button" aria-pressed="true"><i class="fas fa-window-maximize"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col paddingTop">
+        <div class="card h-100 customCard">
+          <img src={weatherApp} class="card-img-top group-img-height" alt="weather_app"/>
+          <div class="card-body group-card-body d-flex flex-column">
+            <h3 class="card-title"><ins>Weather Dashboard</ins></h3>
+            <p class="card-text">This is a weather dashboard made with <b class="moment">OpenWeather Api</b>! Below are the links for the GitHub repository and webpage</p>
+          </div>
+          <div class="card-footer">
+            <div class="text-center mt-auto">
+              <a target="_blank" rel="noreferrer" href="https://github.com/Ashquinn/weather_app1" class="btn p-btn-color btn-lg custom-length-ps" role="button" aria-pressed="true"><b>GitHub </b><i class="fab fa-github-square"></i></a>
+              <a target="_blank" rel="noreferrer" href="https://ashquinn.github.io/weather_app1/" class="btn btn-secondary btn-lg custom-length-ss" role="button" aria-pressed="true"><i class="fas fa-window-maximize"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col paddingTop">
+        <div class="card h-100 customCard">
+          <img src={passGen} class="card-img-top group-img-height" alt="password_generator"/>
+          <div class="card-body group-card-body d-flex flex-column">
+            <h3 class="card-title"><ins>Password Generator</ins></h3>
+            <p class="card-text h">This is a password generator made with <b class="javascript">javascript</b>! Below are the links for the GitHub repository and webpage</p>
+          </div>
+          <div class="card-footer">
+            <div class="text-center mt-auto">
+              <a target="_blank" rel="noreferrer" href="https://github.com/Ashquinn/password_generator" class="btn p-btn-color btn-lg custom-length-ps" role="button" aria-pressed="true"><b>GitHub </b><i class="fab fa-github-square"></i></a>
+              <a target="_blank" rel="noreferrer" href="https://ashquinn.github.io/password_generator/" class="btn btn-secondary btn-lg  custom-length-ss" role="button" aria-pressed="true"><i class="fas fa-window-maximize"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="col paddingTop">
+        <div class="card h-100 customCard">
+          <img src={dayPlanner} class="card-img-top group-img-height" alt="day_planner"/>
+          <div class="card-body group-card-body d-flex flex-column">
+            <h3 class="card-title"><ins>Day Planner</ins></h3>
+            <p class="card-text">This is a day planner using <b class="javascript">Javascript</b> and <b class="moment">moment.js</b>! Below are the links for the GitHub repository and webpage</p>
+          </div>
+          <div class="card-footer">
+            <div class="text-center mt-auto">
+              <a target="_blank" rel="noreferrer" href="https://github.com/Ashquinn/work_day_scheduler" class="btn p-btn-color btn-lg custom-length-ps" role="button" aria-pressed="true"><b>GitHub </b><i class="fab fa-github-square"></i></a>
+              <a target="_blank" rel="noreferrer" href="https://ashquinn.github.io/work_day_scheduler/" class="btn btn-secondary btn-lg custom-length-ss" role="button" aria-pressed="true"><i class="fas fa-window-maximize"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
     </div>
+    <hr></hr>
+  </div>
   );
 }
