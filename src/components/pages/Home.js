fef3d7f7bd4ceb40844e3fc9ea57df90160a5750
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-26 14:07:29 -0400

    class attribute added to h1 tag

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
+      <h1 class="headers main">About Me: Ashquinn Gordon</h1>
+      <p id="about_me">
       Young and aspiring software developer from the Washington Metropolitan area. Currently serving as a secondary classroom teacher with four years of experience in mathematical instruction. Adept at developing rigorous and nurturing learning environment that ensures students’ academic success. Seeking to leverage my background in applied mathematics, curriculum development skills, and peer coaching to transition into a career in the technology field.
       </p>
     </div>
