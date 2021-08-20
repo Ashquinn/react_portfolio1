5fd54ce2d70535e5d7e4e2cb317be36eae8ac67c
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-08-20 15:21:00 -0400

    Home page updated with Brand Statement added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1 class="headers main">About Me: Ashquinn Gordon</h1>
       <p id="about_me">
-      Young and aspiring software developer from the Washington Metropolitan area. Currently serving as a secondary classroom teacher with four years of experience in mathematical instruction. Adept at developing rigorous and nurturing learning environment that ensures students’ academic success. Seeking to leverage my background in applied mathematics, curriculum development skills, and peer coaching to transition into a career in the technology field.
+      Full-stack Web Developer with a background in applied mathematics, curriculum development, and peer coaching. Additionally a former educator with exceptional problem solving ability, organization, and communication. Earned a certificate in web development from George Washington University with skills in agile software development, HTML, CSS, JavaScript,  as well as operating within the scrum process. Participated within group projects to deploy full stack web applications while focusing on delivering an immersive user experience. Was responsible for the data collection and organization by making authenticated API calls to databases. Revels in collaborating alongside group members to accomplish goals and fulfill project demands, and ultimately delivering  polished and seamless applications to the client. As a stout learner with an ambitious character, I will use my culmination of technical skills and synergetic spirit to deliver an indispensable role to your team.
       </p>
     </div>
   );
